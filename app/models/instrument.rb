53a80aa9465ae1c5f4276c9862e0e4f6656812a1
Nicolas Guimier <nicolas.g@flexishore.com>
2019-12-03 17:15:13 +0100

    fix for model instruments

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,7 +1,6 @@
 class Instrument < ApplicationRecord
   has_many :invitations
   has_many :work_instruments
-  has_many :invitations
 
   validates :name, presence: true
   validates :category, presence: true, inclusion: { in: ['Strings', 'Woodwind', 'Brass', 'Percussion'] }
