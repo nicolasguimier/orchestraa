5a78570a0de40a75134e1ed72831f5f3cbdcc614
Nicolas Guimier <nicolas.g@flexishore.com>
2019-12-03 15:27:06 +0100

    concert geocoded

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_130219) do
+ActiveRecord::Schema.define(version: 2019_12_03_133509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "concerts", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "notes"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_concerts_on_user_id"
+  end
+
+  create_table "orchestra_compositions", force: :cascade do |t|
+    t.string "name"
+    t.string "composition"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orchestra_compositions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +52,6 @@ ActiveRecord::Schema.define(version: 2019_12_03_130219) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "concerts", "users"
+  add_foreign_key "orchestra_compositions", "users"
 end
