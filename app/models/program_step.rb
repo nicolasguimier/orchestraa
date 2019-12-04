419cc9c75c67e06038e8e0c57b51a6e701055d7d
Nicolas Guimier <nicolas.g@flexishore.com>
2019-12-04 17:26:44 +0100

    created program steps

--- app/models/program_step.rb
+++ app/models/program_step.rb
@@ -1,6 +1,13 @@
 class ProgramStep < ApplicationRecord
   belongs_to :concert
-  belongs_to :musical_work
+  belongs_to :musical_work, optional: true
 
   validates :kind, inclusion: { in: ['musical_work', 'intermission'] }
+  validates :musical_work, presence: true, if: :musical_step?
+
+  private
+
+  def musical_step?
+    kind == 'musical_work'
+  end
 end
