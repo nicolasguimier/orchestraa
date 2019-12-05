39d425a8597741a3f94567ebd8f5a7ff5461223b
Nicolas Guimier <nicolas.g@flexishore.com>
2019-12-05 01:47:47 +0100

    seed invitations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -4,10 +4,10 @@ class Invitation < ApplicationRecord
 
   validates :full_name, presence: true
   validates :email, presence: true, format: { with: Devise.email_regexp }
-  validates :status, presence: true, inclusion: { in: ['pending', 'accepted', 'accepted'] }
-  validates :sendingblue_email_id, presence: true
-  validates :email_delivered, presence: true
-  validates :email_read, presence: true
-  validates :email_clicked, presence: true
-  validates :email_error, presence: true
+  validates :status, presence: true, inclusion: { in: ['pending', 'accepted', 'rejected'] }
+  validates :sendinblue_email_id, presence: true
+  # validates :email_delivered, presence: true
+  # validates :email_read, presence: true
+  # validates :email_clicked, presence: true
+  # validates :email_error, presence: true
 end
