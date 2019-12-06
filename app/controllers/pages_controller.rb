103f7fa118bf552d83c8fa7053cbfc2be008bb92
Maxime Chenais <maxime.chenais@gmail.com>
2019-12-06 14:01:46 +0100

    musicians card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,36 @@ class PagesController < ApplicationController
   end
 
   def maxime
+    initial_invites_count_per_instrument_category = {
+      "Strings"=>0,
+      "Woodwinds"=>0,
+      "Percussions"=>0,
+      "Keyboard"=>0,
+      "Brass" => 0
+    }
+
+    # For now, hardcoded, we'll see next week if we have time to make it dynamic
+    @total_instruments_needed = {
+      Strings: 12,
+      Woodwinds: 6,
+      Brass: 2,
+      Percussions: 1,
+      Keyboard: 1
+    }
+
+    accepted_invites = Invitation.where(status: :accepted)
+
+    results = accepted_invites.group_by do |invite|
+      invite.instrument.category
+    end
+
+    accepted_invites_count_per_instrument_category = results.map do |instrument_category, invitations|
+      [instrument_category, invitations.count]
+    end
+
+    @accepted_invites_count_per_instrument_category = initial_invites_count_per_instrument_category
+                                                      .merge(accepted_invites_count_per_instrument_category.to_h)
+                                                      .symbolize_keys
   end
 
   def nicolas
