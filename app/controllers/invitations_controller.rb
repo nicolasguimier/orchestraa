75985cdaed59d36d4c62ee254b97b5c7e9e01cb3
Nicolas Guimier <nicolas.g@flexishore.com>
2019-12-07 22:56:12 +0100

    add tabs in invitations index

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,10 @@
 class InvitationsController < ApplicationController
   def index
     @invitations = current_user.invitations.order(id: :desc)
+    @invitations_pending = current_user.invitations.order(id: :desc).where(status: 'pending')
+    @invitations_accepted = current_user.invitations.order(id: :desc).where(status: 'accepted')
+    @invitations_rejected = current_user.invitations.order(id: :desc).where(status: 'rejected')
+
     @invitation = Invitation.new
     @instruments = Instrument.all
   end
@@ -18,14 +22,25 @@ class InvitationsController < ApplicationController
   end
 
   def accept
+    change_status_to('accepted')
+    flash.success = "The invitation is now accepted."
   end
 
   def decline
+    change_status_to('rejected')
+    flash.success = "The invitation is marked as 'Rejected'."
   end
 
   private
 
   def invitation_params
     params.require(:invitation).permit(:instrument_id, :email, :full_name)
+  end
+
+  def change_status_to(string)
+    @invitation = Invitation.find(params[:id])
+    @invitation.status = string
+    @invitation.save
+    redirect_to invitations_path
   end
 end
