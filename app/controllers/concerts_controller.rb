315eacdaf230f4a928c530bb2bac3452801f8036
jeremie rousseau <jere.rousseau@gmail.com>
2019-12-09 17:42:09 +0100

    changed map

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,20 +1,19 @@
 class ConcertsController < ApplicationController
   def index
     @concerts = current_user.concerts.geocoded
-
-    @markers = @concerts.map do |concert|
-      {
-        lat: concert.latitude,
-        lng: concert.longitude,
-        # infoWindow: render_to_string(partial: "info_window", locals: { concert: concert }),
-        # image_url: helpers.asset_url('marker_map.svg')
-      }
-    end
   end
 
   def show
     @concert = Concert.find(params[:id])
     @program_steps = @concert.program_steps.order(position: :asc)
+    @markers = []
+    @markers.push(
+      {
+        lat: @concert.latitude,
+        lng: @concert.longitude,
+        # infoWindow: render_to_string(partial: "info_window", locals: { concert: concert }),
+        image_url: helpers.asset_url('marker_map.svg')
+      })
   end
 
   def update
