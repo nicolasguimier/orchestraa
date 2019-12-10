b9a2f02b93d7e3148f2a4939d467d20f5c6c6285
Nicolas Guimier <nicolas.g@flexishore.com>
2019-12-10 15:25:44 +0100

    Add work instrument in musical work

--- app/controllers/work_instruments_controller.rb
+++ app/controllers/work_instruments_controller.rb
@@ -1,8 +1,9 @@
 class WorkInstrumentsController < ApplicationController
   def create
-    @work_instrument = WorkInstrument.new(work_instr_params)
+    @work_instrument = WorkInstrument.new(work_instrument_params)
+    @work_instrument.musical_work_id = params[:musical_work_id]
     if @work_instrument.save
-      redirect_to musical_work(params[:musical_work])
+      redirect_to musical_work_path(params[:musical_work_id])
     else
       render nothing: true
     end
@@ -16,7 +17,7 @@ class WorkInstrumentsController < ApplicationController
 
   private
 
-  def work_instr_params
-    params.require(:work_instrument).permit(:musical_work)
+  def work_instrument_params
+    params.require(:work_instrument).permit(:instrument_id)
   end
 end
