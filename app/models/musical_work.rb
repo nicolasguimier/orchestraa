781ccf532603ff67dddeb8f87e6d425d5900ab09
Nicolas Guimier <nicolas.g@flexishore.com>
2019-12-11 12:23:20 +0100

    Dynamical missing sheets orange notification

--- app/models/musical_work.rb
+++ app/models/musical_work.rb
@@ -4,4 +4,15 @@ class MusicalWork < ApplicationRecord
   has_many :work_instruments, dependent: :destroy
 
   validates :title, presence: true
+
+  def missing_sheets
+    instruments_list = []
+    work_instruments = self.work_instruments
+    work_instruments.each do |work_instr|
+      unless work_instr.sheet_pdf.url
+        instruments_list << work_instr.instrument.name
+      end
+    end
+    return instruments_list
+  end
 end
